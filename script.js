070116393a0cf21181968777212082ea3d257ad1
rayenmhamdi <rayenmhamdi94@gmail.com>
2019-08-20 00:13:12 +0100

    add presentations 1

---
+++ script.js
@@ -0,0 +1,4 @@
+document.body.oncopy = function() { return false; }
+document.body.oncut = function() { return false; }
+document.body.onpaste = function() { return false; }
+document.body.oncontextmenu = function() { return false; }
