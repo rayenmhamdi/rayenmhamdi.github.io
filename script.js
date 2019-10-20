560e03fcb0395a301fc2c99330f5aece8f81ee18
rayenmhamdi <rayenmhamdi94@gmail.com>
2019-10-20 09:52:36 +0100

    desactivate copy-paste

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-document.body.oncopy = function() { return false; }
+/*document.body.oncopy = function() { return false; }
 document.body.oncut = function() { return false; }
 document.body.onpaste = function() { return false; }
-document.body.oncontextmenu = function() { return false; }
+document.body.oncontextmenu = function() { return false; }*/
